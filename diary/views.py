92102ebe8e022888fe9a02b95d921fbd90cd3951
komugiko11 <ykh2135177@stu.o-hara.ac.jp>
2022-09-15 12:34:01 +0900

    p286まで完成

--- diary/views.py
+++ diary/views.py
@@ -12,6 +12,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Diary
 
+from .forms import InquiryForm, DiaryCreateForm
+
 logger = logging.getLogger(__name__)
 
 class IndexView(generic.TemplateView):
@@ -31,7 +33,29 @@ class InquiryView(generic.FormView):
 class DiaryListView(LoginRequiredMixin, generic.ListView):
     model = Diary
     template_name = 'diary_list.html'
+    paginate_by = 2
 
     def get_queryset(self):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return diaries
+
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Diary
+    template_name = 'diary_detail.html'
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request, '日記を作成しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の作成に失敗しました。")
+        return super().form_invalid(form)
